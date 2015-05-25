4935500193087ed73b0732b9346a2e5b0b4eb0a9
Emily Shenfield <emily.shenfield@gmail.com>
2015-05-24 17:21:25 -0700

    Modified port for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');    
 var methodOverride = require('method-override'); 
 
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname ));                 
 app.use(morgan('dev'));                                         
@@ -12,5 +13,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 app.use(methodOverride());
 
-app.listen(8080);
+app.listen(port);
 console.log("App listening on port 8080");
